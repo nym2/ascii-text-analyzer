0081aaf97b1651e0d997f19971e0f8cd9f2eb609
nym2 <praisepamba@gmail.com>
2026-09-22 11:22:56 +0300

    implements count spaces function

--- src/analyzer.c
+++ src/analyzer.c
@@ -38,3 +38,18 @@ size_t count_digits(const char *text)
 
     return count;
 }
+
+size_t count_spaces(const char *text)
+{
+    size_t count = 0;
+
+    for (size_t i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
