c667d69d084cc6d8c31dc5dc09f0d896da9c5f29
nym2 <praisepamba@gmail.com>
2026-09-22 11:28:43 +0300

    implements count lower case function

--- src/analyzer.c
+++ src/analyzer.c
@@ -68,3 +68,17 @@ size_t count_uppercase(const char *text)
 
     return count;
 }
+
+size_t count_lowercase(const char *text)
+{
+    size_t count = 0;
+
+    for(size_t i = 0; text[i] != '\0'; i++)
+    {
+        if(islower((unsigned char)text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
