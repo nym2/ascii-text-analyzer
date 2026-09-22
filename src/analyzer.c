addc8744b35866bb99e840f69b0bc8845cc5b62f
nym2 <praisepamba@gmail.com>
2026-09-22 11:18:33 +0300

    implement count letters function

--- src/analyzer.c
+++ src/analyzer.c
@@ -3,3 +3,18 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+size_t count_letters(const char *text)
+{
+    size_t count = 0;
+
+    for (size_t i = 0; text[i] != '\0'; i++)
+    {
+        if (isalpha((unsigned char)text[i]))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
